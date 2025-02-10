bbfd200ef4705b2623830212cd8e1d117431138d
Afolabi Abdulsamad <fullendmaestro@gmail.com>
2025-02-10 15:32:33 +0100

    updated next meta data implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,22 @@
-import "./globals.css";
+import type { Metadata } from "next";
 import { Public_Sans } from "next/font/google";
-import { Button } from "@/components/ui/button";
-import { GithubIcon } from "lucide-react";
-import { Toaster } from "@/components/ui/sonner";
+import "./globals.css";
+import { Header } from "@/components/header";
 
 const publicSans = Public_Sans({ subsets: ["latin"] });
 
+export const metadata: Metadata = {
+  title: "Quizy - AI quiz app",
+  description: "AI Quiz Application to help students better practice math",
+};
+
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
-      <head>
-        <title>Quizy - AI quiz app</title>
-        <link rel="shortcut icon" href="/images/favicon.ico" />
-        <meta
-          name="description"
-          content="AI Quiz Application to help students better practice math"
-        />
-        <meta property="og:title" content="LangChain + Next.js Template" />
-        <meta
-          property="og:description"
-          content="AI Quiz Application to help students better practice math"
-        />
-        <meta property="og:image" content="/images/og-image.png" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="LangChain + Next.js Template" />
-        <meta
-          name="twitter:description"
-          content="AI Quiz Application to help students better practice math"
-        />
-        <meta name="twitter:image" content="/images/og-image.png" />
-      </head>
       <body className={publicSans.className}>{children}</body>
     </html>
   );
