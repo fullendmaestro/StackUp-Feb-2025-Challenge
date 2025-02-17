0cf20dd6ef3c0c22da417746ba3e8a1c8681a4bf
Afolabi Abdulsamad <fullendmaestro@gmail.com>
2025-02-17 23:32:41 +0100

    updated missing utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,26 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { Question } from "./db/schema";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function generateUUID(): string {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+    const r = (Math.random() * 16) | 0;
+    const v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
+export function calculateScore(questions: Question[]): any {
+  let score = 0;
+  questions.map((question) => {
+    if (Number(question.submitedAnswer) == question.content.correctAnswer) {
+      score++;
+    }
+  });
+
+  return score;
 }
